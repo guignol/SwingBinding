caf1c687a3cd24e6190e5698050558973f1a8525
Sho Sugimoto <17764785+guignol@users.noreply.github.com>
2018-06-28 18:47:24 +0900

    fix multi subscription issues (from misunderstanding about doOnSubscribe)

--- src/main/java/com/github/guignol/swing/binding/Property.java
+++ src/main/java/com/github/guignol/swing/binding/Property.java
@@ -2,9 +2,10 @@ package com.github.guignol.swing.binding;
 
 import com.github.guignol.swing.rx.SwingScheduler;
 import io.reactivex.Observable;
+import io.reactivex.annotations.Nullable;
+import io.reactivex.disposables.Disposables;
 import io.reactivex.functions.BiPredicate;
 import io.reactivex.functions.Function;
-import io.reactivex.subjects.BehaviorSubject;
 import io.reactivex.subjects.PublishSubject;
 
 import javax.swing.*;
@@ -26,74 +27,71 @@ public class Property {
         HIDDEN
     }
 
-    public static Observable<Event> onEvent(Component component, Event... filter) {
+    public static Observable<Event> onEvent(Component component, @Nullable Event... filter) {
         final List<Event> events = Arrays.asList(filter == null ? Event.values() : filter);
-        final PublishSubject<Event> eventHappened = PublishSubject.create();
-        final ComponentListener eventListener = new ComponentListener() {
+        final boolean notifyWhenResized = events.contains(Event.RESIZED);
+        final boolean notifyWhenMoved = events.contains(Event.MOVED);
+        final boolean notifyWhenShown = events.contains(Event.SHOWN);
+        final boolean notifyWhenHidden = events.contains(Event.HIDDEN);
+        return Observable.<Event>create(emitter -> {
+            final ComponentListener eventListener = new ComponentListener() {
 
-            final boolean notifyWhenResized = events.contains(Event.RESIZED);
-            final boolean notifyWhenMoved = events.contains(Event.MOVED);
-            final boolean notifyWhenShown = events.contains(Event.SHOWN);
-            final boolean notifyWhenHidden = events.contains(Event.HIDDEN);
-
-            @Override
-            public void componentResized(ComponentEvent e) {
-                if (notifyWhenResized) {
-                    eventHappened.onNext(Event.RESIZED);
+                @Override
+                public void componentResized(ComponentEvent e) {
+                    if (notifyWhenResized) {
+                        emitter.onNext(Event.RESIZED);
+                    }
                 }
-            }
 
-            @Override
-            public void componentMoved(ComponentEvent e) {
-                if (notifyWhenMoved) {
-                    eventHappened.onNext(Event.MOVED);
+                @Override
+                public void componentMoved(ComponentEvent e) {
+                    if (notifyWhenMoved) {
+                        emitter.onNext(Event.MOVED);
+                    }
                 }
-            }
 
-            @Override
-            public void componentShown(ComponentEvent e) {
-                if (notifyWhenShown) {
-                    eventHappened.onNext(Event.SHOWN);
+                @Override
+                public void componentShown(ComponentEvent e) {
+                    if (notifyWhenShown) {
+                        emitter.onNext(Event.SHOWN);
+                    }
                 }
-            }
 
-            @Override
-            public void componentHidden(ComponentEvent e) {
-                if (notifyWhenHidden) {
-                    eventHappened.onNext(Event.HIDDEN);
+                @Override
+                public void componentHidden(ComponentEvent e) {
+                    if (notifyWhenHidden) {
+                        emitter.onNext(Event.HIDDEN);
+                    }
                 }
-            }
-        };
-        return eventHappened
-                .doOnSubscribe(disposable -> component.addComponentListener(eventListener))
-                .doOnDispose(() -> component.removeComponentListener(eventListener))
+            };
+            component.addComponentListener(eventListener);
+            emitter.setDisposable(Disposables.fromAction(() -> component.removeComponentListener(eventListener)));
+        })
                 .subscribeOn(SwingScheduler.getInstance())
                 .unsubscribeOn(SwingScheduler.getInstance());
     }
 
     public static Observable<MouseEvent> onClick(Component component) {
-        final PublishSubject<MouseEvent> onClick = PublishSubject.create();
-        final MouseAdapter listener = new MouseAdapter() {
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                onClick.onNext(e);
-            }
-        };
-        return onClick
-                .hide()
-                .doOnSubscribe(disposable -> component.addMouseListener(listener))
-                .doOnDispose(() -> component.removeMouseListener(listener))
+        return Observable.<MouseEvent>create(emitter -> {
+            final MouseAdapter listener = new MouseAdapter() {
+                @Override
+                public void mouseClicked(MouseEvent event) {
+                    emitter.onNext(event);
+                }
+            };
+            component.addMouseListener(listener);
+            emitter.setDisposable(Disposables.fromAction(() -> component.removeMouseListener(listener)));
+        })
                 .subscribeOn(SwingScheduler.getInstance())
                 .unsubscribeOn(SwingScheduler.getInstance());
     }
 
     public static Observable<ActionEvent> onAction(AbstractButton button) {
-        final PublishSubject<ActionEvent> onAction = PublishSubject.create();
-        final ActionListener actionListener = onAction::onNext;
-        return onAction
-                .hide()
-                .doOnSubscribe(disposable -> button.addActionListener(actionListener))
-                .doOnDispose(() -> button.removeActionListener(actionListener))
+        return Observable.<ActionEvent>create(emitter -> {
+            final ActionListener actionListener = emitter::onNext;
+            button.addActionListener(actionListener);
+            emitter.setDisposable(Disposables.fromAction(() -> button.removeActionListener(actionListener)));
+        })
                 .subscribeOn(SwingScheduler.getInstance())
                 .unsubscribeOn(SwingScheduler.getInstance());
     }
@@ -103,124 +101,107 @@ public class Property {
                 .map(documentEvent -> textComponent.getText());
         final PublishSubject<String> initialValue = PublishSubject.create();
         return Observable.merge(onDocumentUpdated.hide(), initialValue)
+                // TODO 繋ぐ度に最新の値を取得したいけど、これで正しい？
                 .doOnSubscribe(disposable -> initialValue.onNext(textComponent.getText()))
                 .subscribeOn(SwingScheduler.getInstance());
     }
 
     public static Observable<DocumentEvent> onDocumentUpdated(Document document) {
-        final BehaviorSubject<DocumentEvent> updated = BehaviorSubject.create();
-        final DocumentListener listener = new DocumentListener() {
-            @Override
-            public void insertUpdate(DocumentEvent event) {
-                updated.onNext(event);
-            }
+        return Observable.<DocumentEvent>create(emitter -> {
+            final DocumentListener listener = new DocumentListener() {
+                @Override
+                public void insertUpdate(DocumentEvent event) {
+                    emitter.onNext(event);
+                }
 
-            @Override
-            public void removeUpdate(DocumentEvent event) {
-                updated.onNext(event);
-            }
+                @Override
+                public void removeUpdate(DocumentEvent event) {
+                    emitter.onNext(event);
+                }
 
-            @Override
-            public void changedUpdate(DocumentEvent event) {
-                updated.onNext(event);
-            }
-        };
-        return updated
-                .hide()
-                .doOnSubscribe(disposable -> document.addDocumentListener(listener))
-                .doOnDispose(() -> document.removeDocumentListener(listener))
+                @Override
+                public void changedUpdate(DocumentEvent event) {
+                    emitter.onNext(event);
+                }
+            };
+            document.addDocumentListener(listener);
+            emitter.setDisposable(Disposables.fromAction(() -> document.removeDocumentListener(listener)));
+        })
                 .subscribeOn(SwingScheduler.getInstance())
                 .unsubscribeOn(SwingScheduler.getInstance());
     }
 
     public static Observable<Integer> onHovered(JList list) {
-        final PublishSubject<Integer> hovered = PublishSubject.create();
-        final MouseAdapter adapter = new MouseAdapter() {
-            private boolean entered = false;
+        return Observable.<Integer>create(emitter -> {
+            final MouseAdapter adapter = new MouseAdapter() {
+                private boolean entered = false;
 
-            @Override
-            public void mouseEntered(MouseEvent e) {
-                entered = true;
-            }
-
-            @Override
-            public void mouseExited(MouseEvent e) {
-                entered = false;
-                hovered.onNext(-1);
-            }
-
-            @Override
-            public void mouseMoved(MouseEvent e) {
-                if (entered) {
-                    hovered.onNext(list.locationToIndex(e.getPoint()));
-                } else {
-                    hovered.onNext(-1);
+                @Override
+                public void mouseEntered(MouseEvent e) {
+                    entered = true;
                 }
-            }
-        };
-        return hovered
-                .hide()
-                .doOnSubscribe(disposable -> {
-                    list.addMouseListener(adapter);
-                    list.addMouseMotionListener(adapter);
-                })
-                .doOnDispose(() -> {
-                    list.removeMouseListener(adapter);
-                    list.removeMouseMotionListener(adapter);
-                })
+
+                @Override
+                public void mouseExited(MouseEvent e) {
+                    entered = false;
+                    emitter.onNext(-1);
+                }
+
+                @Override
+                public void mouseMoved(MouseEvent e) {
+                    if (entered) {
+                        emitter.onNext(list.locationToIndex(e.getPoint()));
+                    } else {
+                        emitter.onNext(-1);
+                    }
+                }
+            };
+            list.addMouseListener(adapter);
+            list.addMouseMotionListener(adapter);
+            emitter.setDisposable(Disposables.fromAction(() -> {
+                list.removeMouseListener(adapter);
+                list.removeMouseMotionListener(adapter);
+            }));
+        })
                 .subscribeOn(SwingScheduler.getInstance())
                 .unsubscribeOn(SwingScheduler.getInstance())
                 .distinctUntilChanged();
     }
 
     public static Observable<int[]> onSelection(JList list) {
-        final BehaviorSubject<int[]> indices = BehaviorSubject.create();
-        final ListSelectionListener selectionListener = e -> {
-            final int[] selectedIndices = list.getSelectedIndices();
-            // distinctUntilChangedだと繋ぎ直したときに流せないので自分で比較する
-            if (!Arrays.equals(indices.getValue(), selectedIndices)) {
-                indices.onNext(selectedIndices);
-            }
-        };
-        return indices
-                .hide()
-                .doOnSubscribe(disposable -> {
-                    list.addListSelectionListener(selectionListener);
-                    indices.onNext(list.getSelectedIndices());
-                })
-                .doOnDispose(() -> list.removeListSelectionListener(selectionListener))
+        return Observable.<int[]>create(emitter -> {
+            final ListSelectionListener selectionListener = e -> emitter.onNext(list.getSelectedIndices());
+            list.addListSelectionListener(selectionListener);
+            // 初期値
+            emitter.onNext(list.getSelectedIndices());
+            emitter.setDisposable(Disposables.fromAction(() -> list.removeListSelectionListener(selectionListener)));
+        })
                 .subscribeOn(SwingScheduler.getInstance())
-                .unsubscribeOn(SwingScheduler.getInstance());
+                .unsubscribeOn(SwingScheduler.getInstance())
+                .distinctUntilChanged();
     }
 
     // TODO column
     public static Observable<int[]> onSelection(JTable table) {
-        final BehaviorSubject<int[]> indices = BehaviorSubject.create();
-        final ListSelectionListener selectionListener = e -> {
-            final int[] selectedIndices = table.getSelectedRows();
-            // distinctUntilChangedだと繋ぎ直したときに流せないので自分で比較する
-            if (!Arrays.equals(indices.getValue(), selectedIndices)) {
-                indices.onNext(selectedIndices);
-            }
-        };
-        return indices
-                .hide()
-                .doOnSubscribe(disposable -> {
-                    table.getSelectionModel().addListSelectionListener(selectionListener);
-                    indices.onNext(table.getSelectedRows());
-                })
-                .doOnDispose(() -> table.getSelectionModel().removeListSelectionListener(selectionListener))
+        return Observable.<int[]>create(emitter -> {
+            final ListSelectionListener selectionListener = e -> emitter.onNext(table.getSelectedRows());
+            final ListSelectionModel selectionModel = table.getSelectionModel();
+            selectionModel.addListSelectionListener(selectionListener);
+            // 初期値
+            emitter.onNext(table.getSelectedRows());
+            emitter.setDisposable(Disposables.fromAction(() -> selectionModel.removeListSelectionListener(selectionListener)));
+        })
                 .subscribeOn(SwingScheduler.getInstance())
-                .unsubscribeOn(SwingScheduler.getInstance());
+                .unsubscribeOn(SwingScheduler.getInstance())
+                .distinctUntilChanged();
     }
 
     public static Observable<ChangeEvent> onChanged(JViewport viewport) {
-        final BehaviorSubject<ChangeEvent> onChanged = BehaviorSubject.create();
-        final ChangeListener changeListener = onChanged::onNext;
-        return onChanged
-                .hide()
-                .doOnSubscribe(disposable -> viewport.addChangeListener(changeListener))
-                .doOnDispose(() -> viewport.removeChangeListener(changeListener))
+        return Observable.<ChangeEvent>create(emitter -> {
+            final ChangeListener changeListener = emitter::onNext;
+            viewport.addChangeListener(changeListener);
+            emitter.setDisposable(Disposables.fromAction(() -> viewport.removeChangeListener(changeListener)));
+        })
                 .subscribeOn(SwingScheduler.getInstance())
                 .unsubscribeOn(SwingScheduler.getInstance());
     }
@@ -233,35 +214,26 @@ public class Property {
     public static <T> Observable<T> onChanged(AbstractButton button,
                                               Function<AbstractButton, T> getter,
                                               BiPredicate<? super T, ? super T> comparer) {
-        final BehaviorSubject<T> propertyChanged = BehaviorSubject.create();
-        final ChangeListener changeListener = e -> {
-            final T property;
-            try {
-                property = getter.apply(button);
-            } catch (Exception exception) {
-                exception.printStackTrace();
-                return;
-            }
-            try {
-                // distinctUntilChangedだと繋ぎ直したときに流せないので自分で比較する
-                if (!comparer.test(propertyChanged.getValue(), property)) {
-                    propertyChanged.onNext(property);
+        return Observable.<T>create(emitter -> {
+            final ChangeListener changeListener = e -> {
+                final T property;
+                try {
+                    property = getter.apply(button);
+                } catch (Exception exception) {
+                    exception.printStackTrace();
+                    emitter.onError(exception);
+                    return;
                 }
-            } catch (Exception exception) {
-                exception.printStackTrace();
-                // いちおう流す
-                propertyChanged.onNext(property);
-            }
-        };
-        return propertyChanged
-                .hide()
-                .doOnSubscribe(disposable -> {
-                    button.addChangeListener(changeListener);
-                    final T property = getter.apply(button);
-                    propertyChanged.onNext(property);
-                })
-                .doOnDispose(() -> button.removeChangeListener(changeListener))
+                emitter.onNext(property);
+            };
+            button.addChangeListener(changeListener);
+            // 初期値
+            final T property = getter.apply(button);
+            emitter.onNext(property);
+            emitter.setDisposable(Disposables.fromAction(() -> button.removeChangeListener(changeListener)));
+        })
                 .subscribeOn(SwingScheduler.getInstance())
-                .unsubscribeOn(SwingScheduler.getInstance());
+                .unsubscribeOn(SwingScheduler.getInstance())
+                .distinctUntilChanged(comparer);
     }
 }
